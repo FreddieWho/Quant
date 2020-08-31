4d011f0f31c4bb54942d86835e43b6a909ad06fc
freddiewho <freddiewho@163.com>
2020-09-01 00:26:04 +0800

    download

--- test_akshare.py
+++ test_akshare.py
@@ -1,10 +1,16 @@
 import akshare as ak
+import pandas as pd
 
 allStockID = ak.stock_zh_a_spot()
+allStockID.to_csv('allStockID.csv')
 
 allHistory = {}
 
 for symbol in allStockID['symbol']:
-    current = stock_zh_a_daily_hfq_df = ak.stock_zh_a_daily(symbol=symbol, adjust="hfq")
+    print(symbol)
+    current = stock_zh_a_daily_hfq_df = ak.stock_zh_a_daily(symbol=symbol, adjust="hfq",)
     allHistory[symbol] = current
 
+allHistory = pd.concat(allHistory)
+
+allHistory.to_json('allHistory.json')
